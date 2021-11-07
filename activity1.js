8a73ae94063d9d89c3f1009394f11ebd052c7653
xndybxy598 <84103424+xndybxy598@users.noreply.github.com>
2021-11-06 19:54:48 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+var score = 0
+
+function updateScore(){
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function saveScore(){
+    localStorage.setItem("score" , score);
+}
+
+function nextPage(){
+    window.location = "activity2.html";
+}
